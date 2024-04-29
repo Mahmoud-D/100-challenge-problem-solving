a36f791e1be29d09b5271cdd44261246079b686f
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-04-29 11:05:43 +0300

    Convert-a-Number-to-a-String

---
+++ Convert-a-Number-to-a-String.js
@@ -0,0 +1,27 @@
+/* Description:
+We need a function that can transform a number (integer) into a string.
+*/
+
+/* 
+problem analysis:
+-1 We have a number
+-2 We need to convert the number to a string
+-3 We need to return the string
+*/
+
+/* 
+steps of solution:
+-1 Convert the number to a string
+-2 Return the string
+*/
+
+function numberToString(num) {
+  return `${num}`;
+}
+
+console.log(numberToString(5));
+
+// another solution
+/* function numberToString(num) {
+  return num.toString();
+} */
